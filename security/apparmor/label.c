aa743486e1fa197a94f2518e15febadf892a1928
John Johansen <john.johansen@canonical.com>
2014-07-25 04:02:09 -0700

    labels are not looked up by name so make sorting and lookup more efficient
    Labels are currently sorted by namespace, and then profile name. However
    we never lookup labels by their name, instead we lookup the profile in
    the namespace, and then use that to lookup the label.
    
    So we can make label sorting and lookup more efficient for compound
    labels by sorting by profile address.
    
    The question of course is whether we want to do this or if we
    want to move profile lookups into the label tree. To do so, the tree
    must allow lockless read (planned), and allow dealing with finding
    profile hierachy efficiently.
    
    need to lookup children, and children of children.
    current design isn't the best for this.  Can't do incremental lookups,
    need full name in advance.
    
    do we want to make the labelset hierachical like profiles?
    - more locking (or maybe single lock still)
    - what of compound labels???  how to find what tree they are in?
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/label.c
+++ security/apparmor/label.c
@@ -438,16 +438,15 @@ static int profile_cmp(struct aa_profile *a, struct aa_profile *b)
 	AA_BUG(!b);
 	AA_BUG(!a->ns);
 	AA_BUG(!b->ns);
-	AA_BUG(!a->base.hname);
-	AA_BUG(!b->base.hname);
 
-	if (a == b || a->base.hname == b->base.hname)
+	if (a == b)
 		return 0;
+
 	res = ns_cmp(a->ns, b->ns);
 	if (res)
 		return res;
 
-	return strcmp(a->base.hname, b->base.hname);
+	return a - b;
 }
 
 /**
