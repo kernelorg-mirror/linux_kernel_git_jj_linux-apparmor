ee8145020d0f93315a502216f57dd4b584533c6c
John Johansen <john.johansen@canonical.com>
2014-07-27 03:25:28 -0700

    fix: all_perms
    quiet and hide are like allow in that they don't accumulate, but get
    reduced by intersection. So they must start fully set if they are
    going to be used in accumulation.
    
    This is not a problem because they are only used when something is
    denied, so if all_perms is used without accum none thing is denied,
    and if used with accumulation they have been reduced.

--- security/apparmor/include/perms.h
+++ security/apparmor/include/perms.h
@@ -88,10 +88,13 @@ struct aa_perms {
 #define ALL_PERMS_MASK 0xffffffff
 
 #define aa_perms_clear(X) memset((X), 0, sizeof(*(X)));
-#define aa_perms_all(X)				\
-	do {					\
-		aa_perms_clear(X);		\
-		(X)->allow = ALL_PERMS_MASK;	\
+#define aa_perms_all(X)						\
+	do {							\
+		aa_perms_clear(X);				\
+		(X)->allow = ALL_PERMS_MASK;			\
+		/* the following are only used for denials */	\
+		(X)->quiet = ALL_PERMS_MASK;			\
+		(X)->hide = ALL_PERMS_MASK;			\
 	} while (0)
 
 #define xcheck(FN1, FN2)	\
