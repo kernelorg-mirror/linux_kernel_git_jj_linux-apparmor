6a2558caed6aacdcb8adeb59659c73ce31a791a7
John Johansen <john.johansen@canonical.com>
2017-01-27 04:36:47 -0800

    apparmor: fixup description of aa_replace_profiles
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/policy.c
+++ security/apparmor/policy.c
@@ -827,8 +827,9 @@ static int __lookup_replace(struct aa_ns *ns, const char *hname,
  * @udata: serialized data stream  (NOT NULL)
  *
  * unpack and replace a profile on the profile list and uses of that profile
- * by any aa_cred_ctx.  If the profile does not exist on the profile list
- * it is added.
+ * by any task creds via invalidating the old version of the profile, which
+ * tasks will notice to update their own cred.  If the profile does not exist
+ * on the profile list it is added.
  *
  * Returns: size of data consumed else error code on failure.
  */
